735b46f395a709944231730f5e55122697178eb4
lucaslos <lucas@sugestly.com>
2022-06-26 08:43:06 -0300

    fix automatic react import in prod

--- src/swc-react-refresh.ts
+++ src/swc-react-refresh.ts
@@ -1,64 +1,83 @@
-import fs from "fs";
-import path from "path";
-import { transform } from "@swc/core";
-import { PluginOption } from "vite";
+import fs from 'fs'
+import path from 'path'
+import { transform } from '@swc/core'
+import { PluginOption } from 'vite'
 
-const runtimePublicPath = "/@react-refresh";
+const runtimePublicPath = '/@react-refresh'
 
 const preambleCode = `import { injectIntoGlobalHook } from "${runtimePublicPath}";
 injectIntoGlobalHook(window);
 window.$RefreshReg$ = () => {};
-window.$RefreshSig$ = () => (type) => type;`;
+window.$RefreshSig$ = () => (type) => type;`
 
-const importReactRE = /import\s+(\*\s+as\s+)?React(,|\s+)/;
+const importReactRE = /import\s+(\*\s+as\s+)?React(,|\s+)/
 
-let define: { [key: string]: string } | undefined;
+let define: { [key: string]: string } | undefined
+let isProduction = false
 
 export default (): PluginOption => ({
-  name: "react-refresh",
-  apply: "serve",
-  config: (config) => {
-    if (config.esbuild) define = config.esbuild.define;
-    config.esbuild = false;
+  name: 'react-refresh',
+  config: (config, { command }) => {
+    isProduction = command === 'build'
+
+    if (!isProduction) {
+      if (config.esbuild) define = config.esbuild.define
+      config.esbuild = false
+    }
   },
-  resolveId: (id) => (id === runtimePublicPath ? id : undefined),
-  load: (id) =>
-    id === runtimePublicPath
-      ? fs.readFileSync(path.join(__dirname, "refresh-runtime.js"), "utf-8")
-      : undefined,
-  transformIndexHtml: () => [
-    { tag: "script", attrs: { type: "module" }, children: preambleCode },
-  ],
+  resolveId: (id) =>
+    !isProduction && id === runtimePublicPath ? id : undefined,
+  load: (id) => {
+    if (isProduction) return
+
+    return id === runtimePublicPath
+      ? fs.readFileSync(path.join(__dirname, 'refresh-runtime.js'), 'utf-8')
+      : undefined
+  },
+  transformIndexHtml: () => {
+    if (isProduction) return
+
+    return [
+      { tag: 'script', attrs: { type: 'module' }, children: preambleCode },
+    ]
+  },
   async transform(code, id) {
-    if (id.includes("node_modules")) return;
-    if (!/\.[jt]sx?$/.test(id)) return;
+    if (id.includes('node_modules')) return
+    if (!/\.[jt]sx?$/.test(id)) return
+
+    if (isProduction) {
+      if (code.includes('React.createElement') && !importReactRE.test(code)) {
+        return { code: `${code}\nimport React from "react";\n` }
+      } else {
+        return
+      }
+    }
 
     const result = await transform(code, {
       filename: id,
       swcrc: false,
       configFile: false,
-
       jsc: {
-        target: "es2020",
+        target: 'es2020',
         transform: {
           react: {
             refresh: true,
             development: true,
             useBuiltins: true,
-            runtime: "classic",
+            runtime: 'classic',
           },
           optimizer: { globals: { vars: define } },
         },
       },
-    });
+    })
 
     if (
-      result.code.includes("React.createElement") &&
+      result.code.includes('React.createElement') &&
       !importReactRE.test(result.code)
     ) {
-      result.code = `\nimport React from "react";\n${result.code}`;
+      result.code = `${result.code}\nimport React from "react";\n`
     }
-    if (!result.code.includes("$RefreshReg$")) return result;
+    if (!result.code.includes('$RefreshReg$')) return result
 
     const header = `import * as RefreshRuntime from "${runtimePublicPath}";
 
@@ -71,14 +90,14 @@ prevRefreshReg = window.$RefreshReg$;
 prevRefreshSig = window.$RefreshSig$;
 window.$RefreshReg$ = RefreshRuntime.getRefreshReg("${id}");
 window.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
-`;
+`
 
     const footer = `
 window.$RefreshReg$ = prevRefreshReg;
 window.$RefreshSig$ = prevRefreshSig;
 import.meta.hot.accept();
-RefreshRuntime.enqueueUpdate();`;
+RefreshRuntime.enqueueUpdate();`
 
-    return { code: `${header}${result.code}${footer}`, map: result.map };
+    return { code: `${header}${result.code}${footer}`, map: result.map }
   },
-});
+})
